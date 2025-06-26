86a66fdf3da7325a6fd7da2b418815e26f240c37
raphaelobinna <raphaelogbonnaya@gmail.com>
2025-06-26 19:51:28 +0300

    added env logging

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,8 @@ WORKDIR /
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
+ENV PYTHONUNBUFFERED=1
+
 COPY . .
 
 EXPOSE 8080
--- main.py
+++ main.py
@@ -1,20 +1,11 @@
 from fastapi import FastAPI, Request, HTTPException, status
 import hmac, hashlib
 from config import settings
-import hmac, hashlib, logging, sys
 
 app = FastAPI()
 
-# Setup logger
-logger = logging.getLogger("tawk")
-handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.INFO)
-logger.setLevel(logging.INFO)
-logger.addHandler(handler)
-logger.propagate = False
 
-
-
+# print(settings.tawk_webhook_secret)
 
 def verify_signature(raw_body: bytes, signature: str) -> bool:
     digest = hmac.new(settings.tawk_webhook_secret.encode(), raw_body, hashlib.sha1).hexdigest()
@@ -28,7 +19,6 @@ async def tawk_webhook(request: Request):
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid signature")
 
     data = await request.json()
-    logger.info("📩 Received Tawk.to payload: %s", data)
     print("📩 Received Tawk.to webhook payload:")
     print(data)
 
